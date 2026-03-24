1683d0af6f726dae0215b320e867759bba88966b
agustinghersi <agusghersi@gmail.com>
2026-03-24 10:12:02 -0300

    Agrego sh para modificar la cantidad de clients

---
+++ mi-generador.py
@@ -0,0 +1,48 @@
+import sys
+import yaml
+
+docker_file = sys.argv[1]
+clients = sys.argv[2]
+
+def main():
+    delete_docker_clients()
+    write_docker_clients()
+
+def delete_docker_clients():
+    with open(docker_file, "r") as f:
+        data = yaml.safe_load(f)
+    services = data.get("services", {})
+    for service_name in list(services.keys()):
+        if service_name.startswith("client"):
+            del services[service_name]
+    data["services"] = services
+    with open(docker_file, "w") as f:
+        yaml.dump(data, f)
+
+
+def write_docker_clients():
+    with open(docker_file, "r") as f:
+        data = yaml.safe_load(f)
+    services = data.get("services", {})
+    for num in range(1, int(clients) + 1):
+        new_client = {
+            "container_name": "client" + str(num),
+            "image": "client:latest",
+            "entrypoint": "/client",
+            "environment": [
+                "CLI_ID=" + str(num),
+                "CLI_LOG_LEVEL=DEBUG"
+            ],
+            "networks": [
+                "testing_net"
+            ],
+            "depends_on": [
+                "server"
+            ]
+        }
+        services["client" + str(num)] = new_client
+    data["services"] = services
+    with open(docker_file, "w") as f:
+        yaml.dump(data, f)
+
+main()
