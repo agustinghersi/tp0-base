a4b706d417db2ef1791e8e1dda7cf5001473f9f8
agustinghersi <agusghersi@gmail.com>
2026-03-25 12:42:14 -0300

    Fix

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,4 +1,4 @@
-response=$(docker run --rm --network=tp0_testing_net subfuzion/netcat sh -c 'echo "hola" | nc server 12345')
+response=$(echo "Hola" | docker run --rm --network=tp0_testing_net subfuzion/netcat server 12345)
 
 echo "Response: [$response]"
 if [ "$response" == "hola" ]; then
