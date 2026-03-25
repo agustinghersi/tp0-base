03ff2fa6dbdd23d46d2b23b6cbcc6e532cdf4df2
agustinghersi <agusghersi@gmail.com>
2026-03-25 13:07:02 -0300

    mayuscula

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,7 +1,7 @@
 response=$(echo "Hola" | docker run --rm -i --network=tp0_testing_net subfuzion/netcat -w 2 server 12345)
 
 echo "Response: [$response]"
-if [ "$response" == "hola" ]; then
+if [ "$response" == "Hola" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
