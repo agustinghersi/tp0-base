818833ee655d367a42359ad4e261d704e9ad7b40
agustinghersi <agusghersi@gmail.com>
2026-03-25 12:39:47 -0300

    fix nombre

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,4 +1,4 @@
-response=$(docker run --rm --network=testing_net subfuzion/netcat sh -c 'echo "hola" | nc server 12345')
+response=$(docker run --rm --network=tp0_testing_net subfuzion/netcat sh -c 'echo "hola" | nc server 12345')
 
 echo "Response: [$response]"
 if [ "$response" == "hola" ]; then
