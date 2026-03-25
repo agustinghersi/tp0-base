ee46c874c573c1afde6908c8d4b8dc9240cdcf9c
agustinghersi <agusghersi@gmail.com>
2026-03-25 17:55:09 -0300

    Agrego parametro olvidado

--- client/common/client.go
+++ client/common/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"time"
+	"context"
 
 	"github.com/op/go-logging"
 )
@@ -51,7 +52,7 @@ func (c *Client) createClientSocket() error {
 }
 
 // StartClientLoop Send messages to the client until some time threshold is met
-func (c *Client) StartClientLoop() {
+func (c *Client) StartClientLoop(ctx context.Context) {
 	// There is an autoincremental msgID to identify every message sent
 	// Messages if the message amount threshold has not been surpassed
 	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
