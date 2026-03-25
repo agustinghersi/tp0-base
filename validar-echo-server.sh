30c263eec2ee183b4a6eed7483493f463814cced
agustinghersi <agusghersi@gmail.com>
2026-03-25 18:15:34 -0300

    Saco el puerto del sh ej3

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,4 +1,7 @@
-response=$(echo "Hola" | docker run --rm -i --network=tp0_testing_net subfuzion/netcat -w 2 server 12345)
+#!/bin/bash
+
+PORT=$(grep SERVER_PORT ./server/config.txt | cut -d '=' -f2 | xargs) # Para eliminar los espacios
+response=$(echo "Hola" | docker run --rm -i --network=tp0_testing_net subfuzion/netcat -w 2 server $PORT)
 
 echo "Response: [$response]"
 if [ "$response" == "Hola" ]; then
